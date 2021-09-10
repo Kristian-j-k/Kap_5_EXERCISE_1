63624b102acd7404bb8d49b2bac54f4a08b090f1
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-09-10 22:42:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Exercise 1, if statement simplyfied
+    int x = 9;
+    int a =0;
+    int b = 10;
+    if (x > a && x < b) {
+                System.out.println("positive single digit number.");
+            }
+        }
+
+        
+    }
+
